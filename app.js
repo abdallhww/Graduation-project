2ca74fa368c938e2a0ce7b14b58bbe64551c3edd
abdallhww <abdallhshtnw@gmail.com>
2025-01-27 19:54:04 +0300

    instul express package and criate server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("first line");
+const http=require("node:http");
+
+const server = http.createServer((req, res) => {
+    const url = req.url;//الاشياء الي بدي ياها 
+    const method = req.method;//الاشياء الي بدي ياها 
+
+    if (url === "/hello" && method === "GET") {
+      res.write("Hello World");
+      return res.end();
+    }
+    
+   if (url === "/admin" && method === "GET") {
+    res.write("Abdallh Wael Shatnawi");
+    return res.end();
+  }
+res.write("Not faond");
+  return res.end();
+});
+server.listen(3000, () => console.log("server is running on port 3000"));

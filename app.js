b90631cac22250d8f2ea374c239c41adfbe10ab0
abdallhww <abdallhshtnw@gmail.com>
2025-01-28 17:49:28 +0300

    edit on app.js

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const http=require("node:http");
+const PORT=3000;
 
 const server = http.createServer((req, res) => {
-    const url = req.url;//الاشياء الي بدي ياها 
-    const method = req.method;//الاشياء الي بدي ياها 
+    //const url = req.url;//الاشياء الي بدي ياها 
+    //const method = req.method;//الاشياء الي بدي ياها 
+    const {url, method}=req;
+
+    console.log(method,url);
 
     if (url === "/hello" && method === "GET") {
-      res.write("Hello World");
+      res.write(`<div> Welcome In Home Page</div>`);
+
       return res.end();
     }
     
    if (url === "/admin" && method === "GET") {
-    res.write("Abdallh Wael Shatnawi");
+    res.write(`<div> Abdallh Wael Shatnawi</div>`);
+
     return res.end();
   }
-res.write("Not faond");
+  res.write(`<div>Not Found</div>`);
+
   return res.end();
 });
-server.listen(3000, () => console.log("server is running on port 3000"));
+server.listen(PORT, () => {
+  console.log("Server is running on http://localhost: "+PORT);
+});

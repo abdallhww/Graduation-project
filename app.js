73d778e5e7017ad0d41d1d77cae5aef1319baa65
abdallhww <abdallhshtnw@gmail.com>
2025-02-05 12:18:51 +0300

    edit style,folder viwes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("views", path.join(__dirname, "./views"));
 // 2- set view engin type
 app.set("view engine", "ejs");
 // 3- use express.static method style عشان اقدر اوصل لا ملفات الي جوا  
-app.use(express.static(path.join(__dirname, "./style")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(bodyParser.json());
 

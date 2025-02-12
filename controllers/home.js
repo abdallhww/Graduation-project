eaf06b1588ea30fbe7947dc1a2f3b1705cdf61ab
abdallhww <abdallhshtnw@gmail.com>
2025-02-12 23:28:38 +0300

    add edit on controler.home and bage brokers

--- controllers/home.js
+++ controllers/home.js
@@ -8,12 +8,14 @@ const Technicalsupport = (req, res,next) => {
 
 
   
-const Brokers = (req, res,next) => {
-    res.render("Brokers", { title: "=>Brokers" });
-    res.end();
+  const Brokers = (req, res, next) => {
+    const query = 'SELECT * FROM brokers';
+    pool.query(query, (err, rows) => {
+      if (err) return next(err); // تمرير الخطأ إلى middleware الخطأ
+      res.render('Brokers', { brokers: rows });
+    });
   };
-  module.exports = {Brokers,Technicalsupport};
-  
+
 
   const Viewproducts = (req, res, next) => {
     pool.query("SELECT * FROM products", (err, rows) => {
@@ -22,17 +24,7 @@ const Brokers = (req, res,next) => {
         return res.status(500).json({ error: "Error fetching products" });
       }
       res.render("Viewproducts", { products: rows });
-    res.end();
+
     });
   };
   module.exports = {Brokers,Technicalsupport,Viewproducts}; 
-  /*const Viewproducts = (req, res, next) => {
-    pool.query("SELECT * FROM products", (err, rows) => {
-      if (err) {
-        console.error('Error fetching products:', err);
-        return res.status(500).json({ error: "Error fetching products" });
-      }
-      res.render("Viewproducts");
-    res.end();
-    });
-  };*/
--- utils/db.js
+++ utils/db.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "graduation-project",
-  password: "AbdallhWael2002*",
+  password: process.env.DB_PASSWORD,
 });
-/*pool.getConnection((err, connection) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to the database:', err.stack);
     return;
   }
   console.log('Connected to the database as ID ' + connection.threadId);
   connection.release(); // إطلاق الاتصال بعد التحقق
-});*/
+});
 
 
 module.exports = { pool };

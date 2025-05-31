39b67a678eee6dc39fb83b6b98d21a26fb19147d
abdallhww <abdallhshtnw@gmail.com>
2025-05-31 23:18:24 +0300

    .

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "graduation-project",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 pool.getConnection((err, connection) => {
   if (err) {

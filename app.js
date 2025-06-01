ce14a48ad36554840b62c6c9b3fa5f7f9a378935
abdallhww <abdallhshtnw@gmail.com>
2025-06-01 04:15:25 +0300

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const db = require("./utils/db");
 const { getPool } = require('./utils/db');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const { likesRoutes } = require('./routes/likes');
 const { usersRouter } = require("./routes/users");
--- utils/db.js
+++ utils/db.js
@@ -2,12 +2,25 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // Create the connection pool. The pool-specific settings are the defaults
-const pool = mysql.createPool({
+/*const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
+
+  DB_PASSWORD=AbdallhWael2002*
+DB_HOST=localhost
+DB_USER=root              
+DB_NAME=graduation-project
+});*/
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
+
 pool.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to the database:', err.stack);
